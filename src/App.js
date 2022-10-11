5bc286e206f8eb870bc22cae816ec4761d9394b4
João Lucas Silva Laurindo Cruz <x529427@rede.sp>
2022-10-11 13:05:04 -0300

    pog

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import footerLogo from './mockups/SVMA.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App min-h-screen bg" onClick={() => {console.log("pog")}}>
+      <div className="inline-flex inset-0 border-b-2 border-[#cc3300] pl-20 text-6xl text-start mt-80">
+        <h1 className='my-2'>
+          Terminal de <strong>Atendimento</strong>
+        </h1>
+      </div>
+      <div className='px-20 my-40'>
+        <h2 className='text-[#cc3300] text-6xl font-bold my-4'>Seja bem-vindo(a)</h2>
+        <p className='text-4xl my-4'>Clique na tela para iniciar</p>
+      </div>
+      <img src={footerLogo} alt="" className='fixed bottom-0 right-0'/>
     </div>
   );
 }
